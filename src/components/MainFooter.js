7620e133a9df0f0e3432b58ce836f81bd2631163
Pietro <gpadilla75@uabc.edu.mx>
2022-06-06 20:48:39 -0700

    rev8

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -7,11 +7,11 @@ export default function MainFooter() {
     <div className="MainFooterDiv">
       <footer className="MainFooter">
         {" "}
-        <SocialIcon style={{ height: 50, width: 50, margin:10}}  fgColor="white" url="https://www.instagram.com/_geraspace/"></SocialIcon>{" "}
-        <SocialIcon style={{ height: 50, width: 50, margin:10}}  fgColor="white" url="https://www.linkedin.com/in/gearpama/"></SocialIcon>{" "}
-        <SocialIcon style={{ height: 50, width: 50, margin:10}}  bgColor="white" fgColor="black" url="https://github.com/geraspace"></SocialIcon>{" "}
-        <button className="BotonCopiar" onClick={CopiarEmail} value="gpadilla75@uabc.edu.mx" id="CopyEmail"><SocialIcon network="email" title="Da click para copiar mi email" style={{ height: 50, width: 50, margin:10}} fgColor="white"></SocialIcon>{" "}</button>
-        <SocialIcon title="My resume" style={{ height: 50, width: 50, margin:10}} fgColor="white" url="https://www.overleaf.com/read/rdtbrcqfhhpr"></SocialIcon>{" "}
+        <SocialIcon style={{ height: 40, width: 40, margin:10}}  fgColor="white" url="https://www.instagram.com/_geraspace/"></SocialIcon>{" "}
+        <SocialIcon style={{ height: 40, width: 40, margin:10}}  fgColor="white" url="https://www.linkedin.com/in/gearpama/"></SocialIcon>{" "}
+        <SocialIcon style={{ height: 40, width: 40, margin:10}}  bgColor="white" fgColor="black" url="https://github.com/geraspace"></SocialIcon>{" "}
+        <button className="BotonCopiar" onClick={CopiarEmail} value="gpadilla75@uabc.edu.mx" id="CopyEmail"><SocialIcon network="email" title="Da click para copiar mi email" style={{ height: 40, width: 40, margin:10}} fgColor="white"></SocialIcon>{" "}</button>
+        <SocialIcon title="My resume" style={{ height: 40, width: 40, margin:10}} fgColor="white" url="https://www.overleaf.com/read/rdtbrcqfhhpr"></SocialIcon>{" "}
       </footer>
     </div>
   );
